19903cc7648eefdf9526b3307fe4d3a338e1b3f5
abrams <abrams>
2006-03-28 02:52:40 +0000

    *** empty log message ***

--- cougaaride/com.cougaarsoftware.cougaar.ide.launcher.ui/src/com/cougaarsoftware/cougaar/ide/launcher/ui/configuration/CougaarMainTab.java
+++ cougaaride/com.cougaarsoftware.cougaar.ide.launcher.ui/src/com/cougaarsoftware/cougaar/ide/launcher/ui/configuration/CougaarMainTab.java
@@ -21,14 +21,29 @@
 
 package com.cougaarsoftware.cougaar.ide.launcher.ui.configuration;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
 import org.eclipse.jdt.internal.debug.ui.IJavaDebugHelpContextIds;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jdt.internal.debug.ui.launcher.LauncherMessages;
+import org.eclipse.jdt.internal.debug.ui.launcher.MainMethodSearchEngine;
 import org.eclipse.jdt.internal.debug.ui.launcher.SharedJavaMainTab;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.eclipse.jdt.ui.IJavaElementSearchConstants;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
@@ -36,6 +51,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.SelectionDialog;
 
 import com.cougaarsoftware.cougaar.ide.core.constants.ICougaarConstants;
 
@@ -51,9 +67,11 @@ public class CougaarMainTab extends SharedJavaMainTab {
 
 	// UI widgets
 	private Button fSearchExternalJarsCheckButton;
+
 	private Button fConsiderInheritedMainButton;
+
 	private Button fStopInMainCheckButton;
-	
+
 	/**
 	 * DOCUMENT ME!
 	 * 
@@ -61,33 +79,39 @@ public class CougaarMainTab extends SharedJavaMainTab {
 	 *            DOCUMENT ME!
 	 */
 	public void createControl(Composite parent) {
-		
+
 		Font font = parent.getFont();
 		Composite comp = new Composite(parent, SWT.NONE);
 		setControl(comp);
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(getControl(), IJavaDebugHelpContextIds.LAUNCH_CONFIGURATION_DIALOG_MAIN_TAB);
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(getControl(),
+				IJavaDebugHelpContextIds.LAUNCH_CONFIGURATION_DIALOG_MAIN_TAB);
 		GridLayout topLayout = new GridLayout();
 		topLayout.verticalSpacing = 0;
 		comp.setLayout(topLayout);
 		comp.setFont(font);
 		createProjectEditor(comp);
 		createVerticalSpacer(comp, 1);
-		fSearchExternalJarsCheckButton = createCheckButton(parent, LauncherMessages.JavaMainTab_E_xt__jars_6); 
+		fSearchExternalJarsCheckButton = createCheckButton(parent,
+				LauncherMessages.JavaMainTab_E_xt__jars_6);
 		GridData gd = new GridData();
 		gd.horizontalSpan = 2;
 		fSearchExternalJarsCheckButton.setLayoutData(gd);
-		fSearchExternalJarsCheckButton.addSelectionListener(getDefaultListener());
-		fConsiderInheritedMainButton = createCheckButton(parent, LauncherMessages.JavaMainTab_22); 
+		fSearchExternalJarsCheckButton
+				.addSelectionListener(getDefaultListener());
+		fConsiderInheritedMainButton = createCheckButton(parent,
+				LauncherMessages.JavaMainTab_22);
 		gd = new GridData();
 		gd.horizontalSpan = 2;
 		fConsiderInheritedMainButton.setLayoutData(gd);
 		fConsiderInheritedMainButton.addSelectionListener(getDefaultListener());
-		fStopInMainCheckButton = createCheckButton(parent, LauncherMessages.JavaMainTab_St_op_in_main_1); 
+		fStopInMainCheckButton = createCheckButton(parent,
+				LauncherMessages.JavaMainTab_St_op_in_main_1);
 		gd = new GridData();
 		fStopInMainCheckButton.setLayoutData(gd);
 		fStopInMainCheckButton.addSelectionListener(getDefaultListener());
-		createMainTypeEditor(comp, LauncherMessages.JavaMainTab_Main_cla_ss__4, new Button[] {fSearchExternalJarsCheckButton, fConsiderInheritedMainButton, fStopInMainCheckButton});
-		
+		createMainTypeEditor(comp, LauncherMessages.JavaMainTab_Main_cla_ss__4,
+				new Button[] { fSearchExternalJarsCheckButton,
+						fConsiderInheritedMainButton, fStopInMainCheckButton });
 
 	}
 
@@ -98,12 +122,14 @@ public class CougaarMainTab extends SharedJavaMainTab {
 		IJavaElement javaElement = getContext();
 		if (javaElement != null) {
 			initializeJavaProject(javaElement, config);
-		}//end if 
+		}// end if
 		else {
-			config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, EMPTY_STRING);
-		}//end else
+			config.setAttribute(
+					IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME,
+					EMPTY_STRING);
+		}// end else
 		initializeMainTypeAndName(javaElement, config);
-		
+
 		config.setAttribute(
 				IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME,
 				ICougaarConstants.COUGAAR_MAIN_CLASS);
@@ -122,13 +148,91 @@ public class CougaarMainTab extends SharedJavaMainTab {
 	}
 
 	protected void handleSearchButtonSelected() {
-		// TODO Auto-generated method stub
-		
+		IJavaProject project = getJavaProject();
+		IJavaElement[] elements = null;
+		if ((project == null) || !project.exists()) {
+			IJavaModel model = JavaCore.create(ResourcesPlugin.getWorkspace()
+					.getRoot());
+			if (model != null) {
+				try {
+					elements = model.getJavaProjects();
+				}// end try
+				catch (JavaModelException e) {
+					JDIDebugUIPlugin.log(e);
+				}
+			}// end if
+		}// end if
+		else {
+			elements = new IJavaElement[] { project };
+		}// end else
+		if (elements == null) {
+			elements = new IJavaElement[] {};
+		}// end if
+		int constraints = IJavaSearchScope.SOURCES;
+		if (fSearchExternalJarsCheckButton.getSelection()) {
+			constraints |= IJavaSearchScope.APPLICATION_LIBRARIES;
+			constraints |= IJavaSearchScope.SYSTEM_LIBRARIES;
+		}// end if
+		IJavaSearchScope searchScope = SearchEngine.createJavaSearchScope(
+				elements, constraints);
+		MainMethodSearchEngine engine = new MainMethodSearchEngine();
+		IType[] types = null;
+		try {
+			types = engine.searchMainMethods(getLaunchConfigurationDialog(),
+					searchScope, fConsiderInheritedMainButton.getSelection());
+		}// end try
+		catch (InvocationTargetException e) {
+			setErrorMessage(e.getMessage());
+			return;
+		}// end catch
+		catch (InterruptedException e) {
+			setErrorMessage(e.getMessage());
+			return;
+		}// end catch
+		SelectionDialog dialog = null;
+		try {
+			dialog = JavaUI.createTypeDialog(getShell(),
+					getLaunchConfigurationDialog(), SearchEngine
+							.createJavaSearchScope(types),
+					IJavaElementSearchConstants.CONSIDER_CLASSES, false, "**"); //$NON-NLS-1$
+		} catch (JavaModelException e) {
+			setErrorMessage(e.getMessage());
+			return;
+		}// end catch
+		dialog.setTitle(LauncherMessages.JavaMainTab_Choose_Main_Type_11);
+		dialog
+				.setMessage(LauncherMessages.JavaMainTab_Choose_a_main__type_to_launch__12);
+		if (dialog.open() == Window.CANCEL) {
+			return;
+		}// end if
+		Object[] results = dialog.getResult();
+		IType type = (IType) results[0];
+		if (type != null) {
+			fMainText.setText(type.getFullyQualifiedName());
+			fProjText.setText(type.getJavaProject().getElementName());
+		}// end if
 	}
 
-	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
-		// TODO Auto-generated method stub
-		
+	public void performApply(ILaunchConfigurationWorkingCopy config) {
+		config.setAttribute(
+				IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, fProjText
+						.getText().trim());
+		config.setAttribute(
+				IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME,
+				fMainText.getText().trim());
+		mapResources(config);
+
+		// attribute added in 2.1, so null must be used instead of false for
+		// backwards compatibility
+		if (fStopInMainCheckButton.getSelection()) {
+			config.setAttribute(
+					IJavaLaunchConfigurationConstants.ATTR_STOP_IN_MAIN, true);
+		}// end if
+		else {
+			config.setAttribute(
+					IJavaLaunchConfigurationConstants.ATTR_STOP_IN_MAIN,
+					(String) null);
+		}// end else
 	}
 
 	public String getName() {
