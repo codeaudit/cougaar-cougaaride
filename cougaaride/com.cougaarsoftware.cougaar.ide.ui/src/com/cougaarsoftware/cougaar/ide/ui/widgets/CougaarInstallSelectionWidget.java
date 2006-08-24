2befc51639f13bf7b45e8364b53fd84c40777552
abrams <abrams>
2006-08-24 18:17:29 +0000

    resolve NPE reported by bug http://cougaar.org/tracker/index.php?func=detail&aid=169&group_id=19&atid=141

--- cougaaride/com.cougaarsoftware.cougaar.ide.ui/src/com/cougaarsoftware/cougaar/ide/ui/widgets/CougaarInstallSelectionWidget.java
+++ cougaaride/com.cougaarsoftware.cougaar.ide.ui/src/com/cougaarsoftware/cougaar/ide/ui/widgets/CougaarInstallSelectionWidget.java
@@ -1,27 +1,25 @@
 /*
  * Cougaar IDE
- *
+ * 
  * Copyright (C) 2003, Cougaar Software, Inc. <tcarrico@cougaarsoftware.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
  */
 
-
 package com.cougaarsoftware.cougaar.ide.ui.widgets;
-
 
 import java.util.Iterator;
 import java.util.Map;
@@ -57,206 +55,207 @@ import com.cougaarsoftware.cougaar.ide.ui.ICougaarInstallChangeListener;
 import com.cougaarsoftware.cougaar.ide.ui.preferences.CougaarPreferencePage;
 import com.cougaarsoftware.cougaar.ide.ui.preferences.CougaarPreferencesMessages;
 
-
 /**
  * DOCUMENT ME!
- *
+ * 
  * @author soster
  */
-public class CougaarInstallSelectionWidget extends Composite
-    implements IAddCougaarDialogRequestor, ICougaarInstallChangeListener {
-    private Button fAddCougaarInstall;
-    private Combo fCougaarCombo;
-    private ICougaarInstallSelectionChangeListener listener;
-    private IProject project;
+public class CougaarInstallSelectionWidget extends Composite implements
+    IAddCougaarDialogRequestor, ICougaarInstallChangeListener {
 
-    /**
-     * DOCUMENT ME!
-     *
-     * @param parent
-     * @param style
-     * @param listener DOCUMENT ME!
-     * @param proj DOCUMENT ME!
-     */
-    public CougaarInstallSelectionWidget(Composite parent, int style,
-        ICougaarInstallSelectionChangeListener listener, IProject proj) {
-        super(parent, style);
-        this.listener = listener;
-        this.project = proj;
-        GridLayout topLayout = new GridLayout();
-        topLayout.numColumns = 3;
-        topLayout.marginWidth = 0;
-        topLayout.marginHeight = 0;
-        this.setLayout(topLayout);
-        
+  private Button fAddCougaarInstall;
 
-        Label cougaarSelectionLabel = new Label(this, SWT.NONE);
-        cougaarSelectionLabel.setText(CougaarPreferencesMessages.getString(
-                "CougaarConfigurationBlock.cougaarVersion"));
-        cougaarSelectionLabel.setLayoutData(new GridData());
+  private Combo fCougaarCombo;
 
-        fCougaarCombo = new Combo(this, SWT.READ_ONLY);
-        String[] cougaarNames = getCougaarVersions();
-        
-        String defaultVersion = CougaarPlugin.getCougaarPreference(project,
-                ICougaarConstants.COUGAAR_VERSION);
-        setValues(defaultVersion);
-        
-        if (cougaarNames.length > 0) {
-            fCougaarCombo.setItems(cougaarNames);
-         
-            int index = fCougaarCombo.indexOf(defaultVersion);
-            if (index < 0) {
-            	index = 0;
-            }
-            fCougaarCombo.select(index);
+  private ICougaarInstallSelectionChangeListener listener;
+
+  private IProject project;
+
+  /**
+   * DOCUMENT ME!
+   * 
+   * @param parent
+   * @param style
+   * @param listener
+   *          DOCUMENT ME!
+   * @param proj
+   *          DOCUMENT ME!
+   */
+  public CougaarInstallSelectionWidget(Composite parent, int style,
+      ICougaarInstallSelectionChangeListener listener, IProject proj) {
+    super(parent, style);
+    this.listener = listener;
+    this.project = proj;
+    GridLayout topLayout = new GridLayout();
+    topLayout.numColumns = 3;
+    topLayout.marginWidth = 0;
+    topLayout.marginHeight = 0;
+    this.setLayout(topLayout);
+
+    Label cougaarSelectionLabel = new Label(this, SWT.NONE);
+    cougaarSelectionLabel.setText(CougaarPreferencesMessages
+        .getString("CougaarConfigurationBlock.cougaarVersion"));
+    cougaarSelectionLabel.setLayoutData(new GridData());
+
+    fCougaarCombo = new Combo(this, SWT.READ_ONLY);
+    String[] cougaarNames = getCougaarVersions();
+
+    String defaultVersion = CougaarPlugin.getCougaarPreference(project,
+        ICougaarConstants.COUGAAR_VERSION);
+    setValues(defaultVersion);
+
+    if (cougaarNames.length > 0) {
+      fCougaarCombo.setItems(cougaarNames);
+
+      if (defaultVersion != null) {
+        int index = fCougaarCombo.indexOf(defaultVersion);
+        if (index < 0) {
+          index = 0;
         }
-
-        
-        fCougaarCombo.addModifyListener(new ModifyListener() {
-                public void modifyText(ModifyEvent evt) {
-                    handleCougaarComboBoxModified();
-                }
-            });
-       
-
-
-        fCougaarCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
-        fAddCougaarInstall = new Button(this, SWT.NONE);
-        fAddCougaarInstall.setText(CougaarUIMessages.getString(
-                "CougaarCapabilityConfigurationPage.newInstallButton"));
-        fAddCougaarInstall.setLayoutData(new GridData());
-        fAddCougaarInstall.addSelectionListener(new SelectionAdapter() {
-                public void widgetSelected(SelectionEvent evt) {
-                    handleAddButtonSelected();
-                }
-            });
-
-        DialogField.createEmptySpace(this, 2);
-
-      
-    }
-
-    /**
-     * called when the user makes a selection from the combo box
-     */
-    protected void handleCougaarComboBoxModified() {
-        String version = fCougaarCombo.getText();
-        listener.handleCougaarInstallSelected(version);
-    }
-
-
-    private void handleAddButtonSelected() {
-        String id = "com.cougaarsoftware.cougaar.ide.ui.preferences.CougaarPreferencePage";
-
-        CougaarPreferencePage page = new CougaarPreferencePage(this);
-        showPreferencePage(id, page);
-        fCougaarCombo.update();
-    }
-
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param name DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
-     */
-    public boolean isDuplicateName(String name) {
-        return false;
-    }
-
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param cougaar DOCUMENT ME!
-     */
-    public void cougaarAdded(ICougaarInstall cougaar) {
-        String[] versions = getCougaarVersions();
-        fCougaarCombo.setItems(versions);
-        setValues(cougaar.getId());
-    }
-
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param removed DOCUMENT ME!
-     */
-    public void cougaarRemoved(ICougaarInstall removed) {
-        String[] versions = getCougaarVersions();
-        fCougaarCombo.setItems(versions);
-    }
-
-
-    /**
-     * get the cougaar versions
-     *
-     * @return an array of currently configured cougaar versions
-     */
-    private String[] getCougaarVersions() {
-        Map versions = CougaarUI.getCougaarLocations();
-        String[] ret = new String[versions.size()];
-        Iterator iter = versions.keySet().iterator();
-        int count = 0;
-        while (iter.hasNext()) {
-            String version = (String) iter.next();
-            ret[count++] = version;
-        }
-
-        return ret;
-    }
-
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param selectedVersion DOCUMENT ME!
-     */
-    protected void setValues(String selectedVersion) {
-        int count = fCougaarCombo.getItemCount();
-
-        for (int i = 0; i < count; i++) {
-            String item = fCougaarCombo.getItem(i);
-            if (item.equals(selectedVersion)) {
-                fCougaarCombo.select(i);
-                break;
-            }
-        }
-    }
-
-
-    private boolean showPreferencePage(String id, IPreferencePage page) {
-        final IPreferenceNode targetNode = new PreferenceNode(id, page);
-
-        PreferenceManager manager = new PreferenceManager();
-        manager.addToRoot(targetNode);
-
-        final PreferenceDialog dialog = new PreferenceDialog(this.getShell(),
-                manager);
-        final boolean[] result = new boolean[] { false };
-        BusyIndicator.showWhile(this.getDisplay(),
-            new Runnable() {
-                public void run() {
-                    dialog.create();
-                    dialog.setMessage(targetNode.getLabelText());
-                    result[0] = (dialog.open() == Window.OK);
-                }
-            });
-        return result[0];
-    }
-
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
-     */
-    public String getSelectedCougaarInstall() {
-        String version = fCougaarCombo.getText();
-        return version;
+        fCougaarCombo.select(index);
+      }
 
     }
+
+    fCougaarCombo.addModifyListener(new ModifyListener() {
+
+      public void modifyText(ModifyEvent evt) {
+        handleCougaarComboBoxModified();
+      }
+    });
+
+    fCougaarCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+    fAddCougaarInstall = new Button(this, SWT.NONE);
+    fAddCougaarInstall.setText(CougaarUIMessages
+        .getString("CougaarCapabilityConfigurationPage.newInstallButton"));
+    fAddCougaarInstall.setLayoutData(new GridData());
+    fAddCougaarInstall.addSelectionListener(new SelectionAdapter() {
+
+      public void widgetSelected(SelectionEvent evt) {
+        handleAddButtonSelected();
+      }
+    });
+
+    DialogField.createEmptySpace(this, 2);
+
+  }
+
+  /**
+   * called when the user makes a selection from the combo box
+   */
+  protected void handleCougaarComboBoxModified() {
+    String version = fCougaarCombo.getText();
+    listener.handleCougaarInstallSelected(version);
+  }
+
+  private void handleAddButtonSelected() {
+    String id = "com.cougaarsoftware.cougaar.ide.ui.preferences.CougaarPreferencePage";
+
+    CougaarPreferencePage page = new CougaarPreferencePage(this);
+    showPreferencePage(id, page);
+    fCougaarCombo.update();
+  }
+
+  /**
+   * DOCUMENT ME!
+   * 
+   * @param name
+   *          DOCUMENT ME!
+   * 
+   * @return DOCUMENT ME!
+   */
+  public boolean isDuplicateName(String name) {
+    return false;
+  }
+
+  /**
+   * DOCUMENT ME!
+   * 
+   * @param cougaar
+   *          DOCUMENT ME!
+   */
+  public void cougaarAdded(ICougaarInstall cougaar) {
+    String[] versions = getCougaarVersions();
+    fCougaarCombo.setItems(versions);
+    setValues(cougaar.getId());
+  }
+
+  /**
+   * DOCUMENT ME!
+   * 
+   * @param removed
+   *          DOCUMENT ME!
+   */
+  public void cougaarRemoved(ICougaarInstall removed) {
+    String[] versions = getCougaarVersions();
+    fCougaarCombo.setItems(versions);
+  }
+
+  /**
+   * get the cougaar versions
+   * 
+   * @return an array of currently configured cougaar versions
+   */
+  private String[] getCougaarVersions() {
+    Map versions = CougaarUI.getCougaarLocations();
+    String[] ret = new String[versions.size()];
+    Iterator iter = versions.keySet().iterator();
+    int count = 0;
+    while (iter.hasNext()) {
+      String version = (String) iter.next();
+      ret[count++] = version;
+    }
+
+    return ret;
+  }
+
+  /**
+   * DOCUMENT ME!
+   * 
+   * @param selectedVersion
+   *          DOCUMENT ME!
+   */
+  protected void setValues(String selectedVersion) {
+    int count = fCougaarCombo.getItemCount();
+
+    for (int i = 0; i < count; i++) {
+      String item = fCougaarCombo.getItem(i);
+      if (item.equals(selectedVersion)) {
+        fCougaarCombo.select(i);
+        break;
+      }
+    }
+  }
+
+  private boolean showPreferencePage(String id, IPreferencePage page) {
+    final IPreferenceNode targetNode = new PreferenceNode(id, page);
+
+    PreferenceManager manager = new PreferenceManager();
+    manager.addToRoot(targetNode);
+
+    final PreferenceDialog dialog = new PreferenceDialog(this.getShell(),
+        manager);
+    final boolean[] result = new boolean[] { false};
+    BusyIndicator.showWhile(this.getDisplay(), new Runnable() {
+
+      public void run() {
+        dialog.create();
+        dialog.setMessage(targetNode.getLabelText());
+        result[0] = (dialog.open() == Window.OK);
+      }
+    });
+    return result[0];
+  }
+
+  /**
+   * DOCUMENT ME!
+   * 
+   * @return DOCUMENT ME!
+   */
+  public String getSelectedCougaarInstall() {
+    String version = fCougaarCombo.getText();
+    return version;
+
+  }
 }
